c989207be3736ce55935e42110372a5948f9fb44
lim6112j <lim6112j@gmail.com>
2021-08-23 22:36:30 +0900

    settings added

---
+++ .emacs.d.pure/elpa/evil-leader-0.4.3/evil-leader-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from evil-leader.el  -*- no-byte-compile: t -*-
+(define-package "evil-leader" "0.4.3" "let there be <leader>" '((evil "0")) :commit "753b01eb4958370ae2226b3780ff31fe157c2852" :authors '(("Michael Markert" . "markert.michael@googlemail.com")) :maintainer '("Michael Markert" . "markert.michael@googlemail.com") :keywords '("evil" "vim-emulation" "leader") :url "http://github.com/cofi/evil-leader")
---
+++ .emacs.d.pure/transient/history.el
@@ -0,0 +1,6 @@
+((magit-commit nil)
+ (magit-dispatch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-push nil)
+ (magit-status-jump nil))
