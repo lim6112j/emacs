3c815655570e522c468ffef3b5aa08650d3851c0
lim6112j <lim6112j@gmail.com>
2022-06-11 19:28:04 +0900

    lsp custom setting added

--- doom/init.el
+++ doom/init.el
@@ -148,7 +148,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +pretty +pandoc +brain)              ; organize your plain life in plain text
+       (org +roam2 +pretty)              ; organize your plain life in plain text
        (php +lsp)               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
