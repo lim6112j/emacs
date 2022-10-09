56bc7fb3297b0bd898698025070aed42c49d14df
byeong cheol lim <lim6112j@gmail.com>
2022-10-09 16:29:15 +0900

    init el made

--- .emacs
+++
@@ -1,8 +0,0 @@
-(setq inhibit-startup-screen t)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)
-(menu-bar-mode -1)
-(setq visual-bell t)
-
-
---
+++ init.el
@@ -0,0 +1,42 @@
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(setq visible-bell t)
+(load-theme 'tango-dark)
+(require 'package)
+(set-face-attribute 'default nil :height 180)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+(use-package command-log-mode)
+(use-package which-key)
+(require 'which-key)
+(which-key-mode 1)
+(use-package swiper :ensure t)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
