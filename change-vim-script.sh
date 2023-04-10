606320d2f1fd72e32bd1cbcf937686fcc62f4787
ben lim <lim6112j@gmail.com>
2023-04-11 08:46:38 +0900

    error resolved

--- change-vim-script.sh
+++ change-vim-script.sh
@@ -6,7 +6,7 @@ then
   mv ~/.local/share/nvim ~/.local/share/nvim.bak.astro
   mv ~/.config/nvim.bak.normal ~/.config/nvim
   mv ~/.local/share/nvim.bak.normal ~/.local/share/nvim
-elif [-d "./.config/nvim.bak.astro"]
+elif [ -d "./.config/nvim.bak.astro" ]
 then
   echo "changing to nvim astro"
   mv ~/.config/nvim ~/.config/nvim.bak.normal
